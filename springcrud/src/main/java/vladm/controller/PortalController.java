0678585c06c0dffac56696a6d2cd922b25b17915
Vlad <56343453+VladMikhalev1999@users.noreply.github.com>
2020-12-04 16:37:44 +0500

    добавлена inmemory-авторизация

--- springcrud/src/main/java/vladm/controller/PortalController.java
+++ springcrud/src/main/java/vladm/controller/PortalController.java
@@ -10,8 +10,38 @@ public class PortalController {
     public String employeesList() { return "employees"; }
 
     @RequestMapping(value = "/statuses")
-    public String statusessList() { return "statuses"; }
+    public String statusesList() { return "statuses"; }
+
+    @RequestMapping(value = "/auth")
+    public String mainPage() { return "auth"; }
+
+    @RequestMapping(value = "/main")
+    public String loginPage() { return "main"; }
 
     @RequestMapping(value = "")
-    public String mainPage() { return "main"; }
+    public String main() { return "main"; }
+
+    @RequestMapping(value = "/statuses/addStatus")
+    public String showAddStatusForm() { return "addStatus"; }
+
+    @RequestMapping(value = "/statuses/removeStatus")
+    public String showRemoveStatusForm() { return "removeStatus"; }
+
+    @RequestMapping(value = "/statuses/setStatus")
+    public String showUpdateStatusForm() { return "setStatus"; }
+
+    @RequestMapping(value = "/employees/addEmployee")
+    public String showAddEmployeeForm() { return "addEmployee"; }
+
+    @RequestMapping(value = "/employees/removeEmployee")
+    public String showRemoveEmployeeForm() { return "removeEmployee"; }
+
+    @RequestMapping(value = "/employees/setEmployee")
+    public String showUpdateEmployeeForm() { return "setEmployee"; }
+
+    @RequestMapping(value = "/employees/addEmployeeStatus")
+    public String showAddEmpStatForm() { return "addEmployeeStatus"; }
+
+    @RequestMapping(value = "/employees/removeEmployeeStatus")
+    public String showRemoveEmpStatForm() { return "removeEmployeeStatus"; }
 }
--- springcrud/src/main/java/vladm/repository/EmployeeRepository.java
+++ springcrud/src/main/java/vladm/repository/EmployeeRepository.java
@@ -1,7 +1,13 @@
 package vladm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import vladm.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Employee SET name = :name WHERE id = :id")
+    void update(@Param("id") Integer id, @Param("name") String name);
 }
